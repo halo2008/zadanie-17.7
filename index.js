a7ab3ae25220659af9fdfd9bc82c308b8a6d9d44
Konrad Sedkowski <ksedkowski@gmail.com>
2018-10-15 13:31:53 +0200

    add new line for each file

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var fs = require('fs');
 var colors = require('colors');
 
 fs.readdir('./modules/', 'utf-8', function(err, files) {
-    console.log('Dane przed zapisem!'.blue);
-    console.log(files);
-    fs.writeFile('./list.txt', files ,'utf-8', function(err) {
-        if (err) throw err;
-        console.log('Zapisano!'.blue);
-    });
+	console.log('Dane przed zapisem!'.blue);
+	files.forEach(function(file){		
+		console.log(file);
+		fs.appendFile('./list.txt', file + '\n' ,'utf-8', function(err) {
+        	if (err) throw err;        	
+    	});
+	});
+   console.log('Zapisano!'.blue);    
 });
